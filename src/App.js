973a70760eaa06a096b3877aeb860e1cacd32347
David Chen <davidc3287@gmail.com>
2023-07-30 20:36:54 -0700

    add math ops base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
 function App() {
-  const [selected, setSelected] = useState(null);
+  const [selectedNumber, setSelectedNumber] = useState(null);
+  const [selectedOp, setSelectedOp] = useState(null);
   const values = [2, 3, 4, 6];
+  const operations = ["+", "-", "x", "÷"];
 
-  const handleClick = (idx) => {
-    setSelected(idx);
+  const handleNumberClick = (idx) => {
+    setSelectedNumber(idx);
+  };
+
+  const handleOpClick = (idx) => {
+    setSelectedOp(idx);
   };
 
   return (
@@ -15,15 +21,30 @@ function App() {
           <div
             key={idx}
             className={`flex items-center justify-center p-4 rounded-md text-5xl border aspect-square ${
-              selected === idx ? "bg-blue-200" : "bg-gray-200"
+              selectedNumber === idx ? "bg-blue-200" : "bg-gray-200"
             }`}
-            onClick={() => handleClick(idx)}
+            onClick={() => handleNumberClick(idx)}
           >
             {num}
           </div>
         ))}
       </div>
-      <button onClick={handleClick}>Call API</button>
+      <div className="grid grid-cols-4 gap-4 m-3">
+        {operations.map((op, idx) => (
+          <div
+            key={idx}
+            className={`flex items-center justify-center p-4 rounded-md text-3xl border aspect-square ${
+              selectedOp === idx ? "bg-blue-200" : "bg-gray-200"
+            }`}
+            onClick={() => handleOpClick(idx)}
+          >
+            {op}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => console.log(selectedNumber, selectedOp)}>
+        Call API
+      </button>
     </div>
   );
 }
